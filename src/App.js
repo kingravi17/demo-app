8735680a08985094a19be126606e9e68f56c9bed
Ravi Kumar <rkumrpb@amazon.com>
2022-03-23 08:51:05 -0700

    Updating app test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1> Hello From Session - Updated Deployment </h1>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
